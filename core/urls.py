08f3e8b6156e312d5ee83e95504cc2cd75e8230d
Tolu Joel <toluisjoel@gmail.com>
2024-12-29 06:40:56 +0100

    feat: Add viewset for Question queryset

--- core/urls.py
+++ core/urls.py
@@ -19,4 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    path('api/questions/', include('questions.urls')),
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Question
+from .serializers import QuestionSerializer
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
