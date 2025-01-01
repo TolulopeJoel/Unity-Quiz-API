1aa8bf1820e979ce116a02ee5f40fd3c0498e79c
Tolu Joel <toluisjoel@gmail.com>
2025-01-01 14:34:33 +0100

    feat: Add `questions/json` endpoint

--- questions/views.py
+++ questions/views.py
@@ -1,9 +1,14 @@
-from rest_framework import viewsets
+from rest_framework import generics, viewsets
 
 from .models import Question
 from .serializers import QuestionSerializer
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class QuestionListAPIView(generics.ListAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
